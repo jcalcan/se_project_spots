a25fd50770f5907c3af5ab9fcb766cf3eed2bae2
JUAN ALCANTARA <2481328@gmail.com>
2024-10-15 12:27:29 -0400

    optimized margins and refactored JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,28 @@
 const initialCards = [
-  (obj1 = {
+  {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
-  }),
-  (obj2 = {
+  },
+  {
     name: "Restaurant terrace",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
-  }),
-  (obj3 = {
+  },
+  {
     name: "An outdoor cafe",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
-  }),
-  (obj4 = {
+  },
+  {
     name: "A very long bridge, over the forest and through the trees",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-  }),
-  (obj5 = {
+  },
+  {
     name: "Tunnel with morning light",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-  }),
-  (obj6 = {
+  },
+  {
     name: "Mountain house",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-  }),
+  },
 ];
 
 const profileEditButton = document.querySelector(".profile__edit-btn");
@@ -34,7 +34,7 @@ const profileDescriptionInput = editProfileModal.querySelector("#description");
 
 const profileUserName = document.querySelector(".profile__name");
 const profileUserDescription = document.querySelector(".profile__description");
-const submitModalButton = editProfileModal.querySelector(".modal__submit-btn");
+const saveModalButton = editProfileModal.querySelector(".modal__submit-btn");
 
 let cardContentContainer = document.querySelector(".cards__pics");
 
@@ -48,7 +48,7 @@ function closeModal() {
   editProfileModal.classList.remove("modal_opened");
 }
 
-function inputNameDescriptionFormSubmit(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault();
 
   profileUserName.textContent = profileNameInput.value;
@@ -65,14 +65,15 @@ function getCardElement(data) {
   cardElement.querySelector(".card__image").src = data.link;
   cardElement.querySelector(".card__image").alt = data.name;
   cardElement.querySelector(".card__footer-title").textContent = data.name;
-  cardContentContainer.append(cardElement);
+
+  return cardElement;
 }
 
 profileEditButton.addEventListener("click", openModal);
 
 closeModalButton.addEventListener("click", closeModal);
-submitModalButton.addEventListener("click", inputNameDescriptionFormSubmit);
+saveModalButton.addEventListener("click", handleProfileFormSubmit);
 
 for (let i = 0; i < initialCards.length; i++) {
-  getCardElement(initialCards[i]);
+  cardContentContainer.append(getCardElement(initialCards[i]));
 }
